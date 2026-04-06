9a928c84b96b5baa5d8f820f454dd2276a6bd609
Aayush Pratap Singh <aayush.25scs1003003949@iilm.edu>
2026-04-06 23:58:18 +0530

    Refactor water usage tracking and chart updates

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 let usageData = [90, 100, 110];
 
+let chart;
+
+window.onload = function () {
+    let ctx = document.getElementById("myChart").getContext("2d");
+
+    chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: usageData.map((_, i) => i + 1),
+            datasets: [{
+                label: "Water Usage",
+                data: usageData,
+                borderColor: "#0077b6",
+                backgroundColor: "rgba(0,119,182,0.2)",
+                tension: 0.4,
+                fill: true
+            }]
+        }
+    });
+
+    checkLeakage();
+};
+
 function addUsage() {
-    let newUsage = Math.floor(Math.random() * 100) + 80;
+    let newUsage;
+
+    // 30% chance of leakage spike
+    if (Math.random() > 0.7) {
+        newUsage = 200;
+    } else {
+        newUsage = Math.floor(Math.random() * 40) + 80;
+    }
+
     usageData.push(newUsage);
 
     document.getElementById("usage").innerText = newUsage;
 
+    updateChart();
     checkLeakage();
+}
+
+function updateChart() {
+    chart.data.labels = usageData.map((_, i) => i + 1);
+    chart.data.datasets[0].data = usageData;
+    chart.update();
 }
 
 function checkLeakage() {
